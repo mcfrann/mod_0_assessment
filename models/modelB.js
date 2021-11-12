942a00affcab4d6019889394882f5c249e97a4d6
Francesca McConnell <fmcconnell59@gmail.com>
2021-11-12 15:19:32 -0700

    Add object to model B

---
+++ models/modelB.js
@@ -0,0 +1,13 @@
+var house = {
+  color: "gray",
+  style: "modern",
+  bedrooms: 3,
+  bathrooms: 2,
+  hasYard: true,
+  hasFence: true,
+  features: ["granite countertops", "hardwood floors", "natural light"],
+  nearbyAttractions: ["park", "running trail", "nightlife"],
+  address: {street: "Logan", number: 987, city: "Chicago", state: "Illinois"}
+}
+
+console.log(house);
