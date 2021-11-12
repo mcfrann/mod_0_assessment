5fb9441f7cf062b2f67ffd573d4584056bced834
Francesca McConnell <fmcconnell59@gmail.com>
2021-11-12 15:11:35 -0700

    Add object to model A

---
+++ models/modelA.js
@@ -0,0 +1,13 @@
+var laptop = {
+  brand: "Apple",
+  model: "Macbook Air",
+  sizeInInches: 13,
+  year: 2020,
+  new: true,
+  diskDrive: false,
+  programs: ["Chrome", "Skype", "Atom"],
+  addOns: ["extra storage", "external disk drive", "airpods"],
+  storeAddress: {street: "1st Avenue", number: 234, city: "Denver", state: "Colorado"}
+}
+
+console.log(laptop);
