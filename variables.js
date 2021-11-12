760115b29a3b5303701a6d8614e56d11d99777fa
Francesca McConnell <fmcconnell59@gmail.com>
2021-11-12 14:58:32 -0700

    Add variables

---
+++ variables.js
@@ -0,0 +1,14 @@
+var animal = "tiger";
+var isCuddly = false;
+
+animal = "panda";
+
+var num8 = 3;
+var num3 = 9;
+var quotient = num3 / num8
+console.log(quotient);
+
+var favoriteFilms = ["Midsommar", "Interstellar", "Braveheart"];
+favoriteFilms.shift();
+
+console.log(animal.length);
